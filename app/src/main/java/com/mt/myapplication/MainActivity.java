f7d33847a6594a8f7c8dc759c7afc413894b6f61
MT <maotong@putao-inc.com>
2020-04-15 16:53:09 +0800

    添加 flutter 模块

--- app/src/main/java/com/mt/myapplication/MainActivity.java
+++ app/src/main/java/com/mt/myapplication/MainActivity.java
@@ -1,14 +1,20 @@
 package com.mt.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.flutter.embedding.android.FlutterFragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FlutterFragment flutterFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flutterFragment = new FlutterFragment.NewEngineFragmentBuilder().initialRoute("route_contact").build();
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, flutterFragment).commit();
     }
 }
